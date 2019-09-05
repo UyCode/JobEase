7ddc491bd26a57b253fe588d3a69abc1925f2031
UyCode <uyghursoftware@outlook.com>
2019-09-05 09:31:37 +0800

    write README.md permanently

--- dist/store/types/index.js
+++ dist/store/types/index.js
@@ -1,1 +1,13 @@
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0});var _login=require("./login.js");Object.keys(_login).forEach(function(e){"default"!==e&&"__esModule"!==e&&Object.defineProperty(exports,e,{enumerable:!0,get:function(){return _login[e]}})});
+"use strict";
+Object.defineProperty(exports, "__esModule", {
+    value: !0
+});
+var _login = require("./login.js");
+Object.keys(_login).forEach(function(e) {
+    "default" !== e && "__esModule" !== e && Object.defineProperty(exports, e, {
+        enumerable: !0,
+        get: function() {
+            return _login[e]
+        }
+    })
+});
